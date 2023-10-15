f9f420943788e5ee4759a00cb14ffb42b129faa3
Vimukthi <karunarathnevimukthi@gmail.com>
2023-10-15 12:14:37 +0530

    other personal details

--- vimukthi.py
+++ vimukthi.py
@@ -1,2 +1,11 @@
 name = input("Enter your name: ")
 print("Hello, " + name)
+
+address = input("Enter your address: ")
+telephone_number = input("Enter your telephone number: ")
+nic_number = input("Enter your NIC number: ")
+
+
+print("Address: " + address)
+print("Telephone Number: " + telephone_number)
+print("NIC Number: " + nic_number)
