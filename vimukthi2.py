2758717b7b5d4d2fbb5555d603b73f1c381e3bec
Vimukthi <karunarathnevimukthi@gmail.com>
2023-10-15 12:33:32 +0530

    Education Details

--- vimukthi2.py
+++ vimukthi2.py
@@ -1,1 +1,16 @@
-hi
+# Display education details header
+print("Education Details")
+
+
+School_name = input("Enter your school name: ")
+OL_result = input("Enter your O/L result: ")
+AL_index_number = input("Enter your A/L index number: ")
+AL_result = input("Enter your A/L result: ")
+
+
+print("\nEducation Details:")
+print("School Name: " + School_name)
+print("O/L Result: " + OL_result)
+print("A/L Index Number: " + AL_index_number)
+print("A/L Result: " + AL_result)
+
